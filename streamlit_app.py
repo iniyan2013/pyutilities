63039ad04d5d2ac359a26543ad88941a791d145c
iniyan2013 <iniyan.jk@outlook.com>
2024-05-13 21:53:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,5 @@ import streamlit as st
 st.title('PyUtilities')
 
 st.subheader('Raw data')
+st.write("Hi eveyone this is my first web aplication, Pyutilities. The pyutilities web aplication is a collection of usefull mathematical tools such as perimeter, area, pythogoras and etc.")
 
