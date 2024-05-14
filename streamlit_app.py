9e81d6887a78d46f7f1f68c765c9a45650893ac5
iniyan2013 <iniyan.jk@outlook.com>
2024-05-14 08:08:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import math as m
 
 st.title('PyUtilities')
 st.write("Hi eveyone this is my first web aplication, Pyutilities. The pyutilities web aplication is a collection of usefull mathematical tools such as perimeter, area, pythogoras and etc.")
@@ -17,7 +18,7 @@ with st.form("my_form_rect"):
        st.write("perimeter  is: ",rect_perimeter)
 
 
-with st.form("my_form"):
+with st.form("my_form_square"):
    st.subheader('Square')
    square_side =st.number_input("Side length", min_value=None, max_value=None, value="min",  label_visibility="visible")
    
@@ -31,6 +32,25 @@ with st.form("my_form"):
        st.write("Area is: ",square_area)
        square_perimeter = square_side*4
        st.write("perimeter  is: ",square_perimeter)
+
+with st.form("my_form_pythogoras"):
+   st.subheader('pythogoras')
+   rigtanle_triangle_height =st.number_input("height", min_value=None, max_value=None, value="min",  label_visibility="visible")
+   rigtanle_triangle_width =st.number_input("width", min_value=None, max_value=None, value="min",  label_visibility="visible")
+   # Every form must have a submit button.
+   submitted = st.form_submit_button("caclulate")
+
+   if submitted:
+       height_square = rigtanle_triangle_height**2
+       width_square = rigtanle_triangle_width**2
+       square_of_hypotenuse = height_square + width_square
+       hypotenuse = m.sqrt(square_of_hypotenuse)
+       st.write('hypotenuse:', hypotenuse)
+       rigtanle_triangle_area = (rigtanle_triangle_height*rigtanle_triangle_width)/2
+       st.write("Area is: ",rigtanle_triangle_area)
+       rigtanle_triangle_perimeter = rigtanle_triangle_height+rectangle_width+hypotenuse
+       st.write("perimeter  is: ",rigtanle_triangle_perimeter)
+
 
 
 
